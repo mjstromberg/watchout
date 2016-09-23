5a9ab2e47eb0d7b62716ea2d8888c38dee8385bc
mjstromberg <michaeljstromberg@gmail.com>
2016-09-23 16:13:40 -0700

    Draw the enemies on the board

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,51 @@
 // start slingin' some d3 here.
+var gameSettings = {
+  enemyNum: 10,
+  boardSize: 800,
+  enemySize: 50,
+  speed: 1000
+};
+
+// Enemies
+var enemies = new Array(gameSettings.enemyNum).fill('x'); // x values
+
+// update function that takes an array of enemies
+var updateEnemyLocations = function(data) {
+  // add the new added enemies, in most cases its only for the first round
+  var selection = d3.select('.board').selectAll('.enemy').data(data);
+
+  // random location helper function
+  var randomLocation = function() {
+    return Math.floor(Math.random() * (gameSettings.boardSize - gameSettings.enemySize)) + gameSettings.enemySize;
+  };
+
+  // new Elements needs a location
+  selection.enter().append('svg')
+    .attr('class', 'enemy')
+    .style('top', function(enemy) {
+      return randomLocation() + 'px';
+    })
+    .style('left', function(enemy) {
+      return randomLocation() + 'px';
+    });
+
+  // for all the old elements, update location
+  selection
+    // transition
+    .transition()
+      .duration(gameSettings.speed)
+    // css top = random
+    .style('top', function(enemy) {
+      return randomLocation() + 'px';
+    })
+    // css left = random
+    .style('left', function(enemy) {
+      return randomLocation() + 'px';
+    }); 
+};  
+
+//calling the update function with the starting enemies at specified time interval
+setInterval(function() {
+  updateEnemyLocations(enemies);
+}, gameSettings.speed);
+
