5c42849b1ef9e701bba29e6ea8c2e2a5a218ef3d
mjstromberg <michaeljstromberg@gmail.com>
2016-09-23 17:05:44 -0700

    Create a TODO list and a player

--- client/watchout.js
+++ client/watchout.js
@@ -2,11 +2,9 @@
 var gameSettings = {
   enemyNum: 10,
   boardSize: 800,
-  enemySize: 80,
-  speed: 1000
+  enemySize: 50,
+  speed: 2000
 };
-
-// Append board svg to 
 
 // Enemies
 var enemies = new Array(gameSettings.enemyNum).fill('x'); // x values
@@ -51,3 +49,36 @@ setInterval(function() {
   updateEnemyLocations(enemies);
 }, gameSettings.speed);
 
+/*
+
+  TODO
+
+  x Build the player, with css
+    - make it spawn with click
+  - Make the player draggable
+  - Calculate collisions
+  - Display score and timers
+  - (DONE)
+  - Increase number of enemies
+  - Stop the game on collision, and make it static (enemies and player)
+    - Create a reset button
+  - Create game theme and style css accordingly
+
+  GAME MECHANICS
+
+  - The board gets populated with N enemies, static, non-moving
+  - The player clicks the board where its not an enemy to spawn it's sircle and the game starts
+  - The enemies start moving, and the score counter increases as long as no collisions happens
+  - When a collision happens
+    - Collision counter increases
+    - High score gets calculated
+    - Current score sets to 0
+    - Enemies stop moving
+    - Player can't move it's marker
+  
+  IDEAS
+
+  - Increasing number of enemies
+  - Increase rate of points for each rounds
+  - on('Game Over') player resets the board with click on a button and that will activate the board
+*/
